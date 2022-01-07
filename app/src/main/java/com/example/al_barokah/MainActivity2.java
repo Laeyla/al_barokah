7d1a8e7066e9c50f87010a82bd6a69c8667879d8
Laeyla <111202012653@mhs.dinus.ac.id>
2022-01-07 23:24:21 +0700

    Bismillah

---
+++ app/src/main/java/com/example/al_barokah/MainActivity2.java
@@ -0,0 +1,70 @@
+package com.example.al_barokah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if (total >=200000){
+        txtbonus.setText("Bonus: Ayam Putih 2kg");
+    } else if (total >=500000){
+        txtbonus.setText("Bonus: Ayam Putih 2kg");
+    } else if (total >=400000){
+        txtbonus.setText("Bonus: Ayam Putih 2kg");
+    } else{
+        txtbonus.setText("Bonus: tidak ada");
+    }
+
+    private Object total;
+    double uangkembalikan = (ub - total;)
+
+    if (ub < total) {
+        txtketerangan.setText("Maaf yang Anda bayarkan kurang");
+        txtkembali.setText(Unang Kembali Rp.0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
